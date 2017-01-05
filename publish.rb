f92a61c763d5c19ff4ddfc9fec20f3b2b10a429c
Mark Pearl <mark.pearl@myob.com>
2017-01-06 07:54:34 +1300

    refactor

--- publish.rb
+++ publish.rb
@@ -7,67 +7,71 @@ LAMBDA_NAME = "LambdaHelloWolrdExample"
 AWS_REGION = 'ap-southeast-2'
 AWS_IAM_ROLE = 'arn:aws:iam::304434685916:role/service-role/lambda_basic_execution';
 
-def zipProject()
-    FileUtils.rm_rf('index.zip')
-    Dir.chdir('lambda')
-    `zip -X -r ../index.zip *`
-    Dir.chdir('..')
-    file = File.open("index.zip", "rb")
-    contents = file.read
-    file.close()
-    return contents;
+class LambdaPublisher
+    
+    def initialize
+       @@client = Aws::Lambda::Client.new(region: AWS_REGION)
+       @@logger = Logger.new(STDOUT)
+       zipSourceCode
+    end
+
+    def update()
+        if functionExists(LAMBDA_NAME)
+           updateFunction
+            @@logger.info("function updated")
+        else
+            createFunction
+            @@logger.info("function created")
+        end
+    end
+
+    private 
+
+        def zipSourceCode()
+            FileUtils.rm_rf('index.zip')
+            Dir.chdir('lambda')
+            `zip -X -r ../index.zip *`
+            Dir.chdir('..')
+        end
+
+        def loadZippedSourceCode()
+            file = File.open("index.zip", "rb")
+            contents = file.read
+            file.close()
+            return contents;
+        end
+
+
+        def updateFunction
+            result = @@client.update_function_code({
+                function_name: LAMBDA_NAME,
+                zip_file: loadZippedSourceCode
+            })
+        end
+
+        def createFunction
+            result = @@client.create_function({
+                function_name: LAMBDA_NAME,
+                runtime: "nodejs4.3",
+                role: AWS_IAM_ROLE,
+                handler: "index.handler",
+                code: {
+                    zip_file: loadZippedSourceCode
+                },
+                publish: true
+            })
+        end
+
+        def getFunctionNames
+            functions = @@client.list_functions
+            functions.functions.map { |x| x.function_name }
+        end
+
+        def functionExists(functionName)
+            getFunctionNames.include?(functionName);
+        end
+
 end
 
-def deleteLambdaFunction
-    client = Aws::Lambda::Client.new(region: AWS_REGION)
-
-    result  = client.delete_function({
-        function_name: LAMBDA_NAME
-    })
-end
-
-def updateLambdaFunction
-    client = Aws::Lambda::Client.new(region: AWS_REGION)
-
-    result  = client.update_function_code({
-        function_name: LAMBDA_NAME,
-        zip_file: zipProject()
-    })
-end
-
-def createLambdaFunction
-
-    client = Aws::Lambda::Client.new(region: AWS_REGION)
-
-    result = client.create_function({
-        function_name: LAMBDA_NAME,
-        runtime: "nodejs4.3",
-        role: AWS_IAM_ROLE,
-        handler: "index.handler",
-        code: {
-            zip_file: zipProject
-        },
-        publish: true
-    })
-end
-
-def getLambdaFunctionNames
-    client = Aws::Lambda::Client.new(region: AWS_REGION)
-    functions = client.list_functions
-    functions.functions.map { |x| x.function_name }
-end
-
-def lambdaFunctionExists(functionName)
-    getLambdaFunctionNames.include?(functionName);
-end
-
-puts "starting..."
-
-if lambdaFunctionExists(LAMBDA_NAME)
-   puts 'updating...'
-   updateLambdaFunction
-else
-    puts 'creating...'
-    createLambdaFunction
-end
-puts "done..."
+publisher = LambdaPublisher.new
+publisher.update
