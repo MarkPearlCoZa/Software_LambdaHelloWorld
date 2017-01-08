50ce2c48aa8882ead07a9b74c3aaa55f79387577
Mark Pearl <mark.pearl@myob.com>
2017-01-08 15:56:13 +1300

    create role if not existing

--- publish.rb
+++ publish.rb
@@ -3,7 +3,7 @@
 require 'io/console'
 require 'aws-sdk'
 
-AWS_ROLE_NAME = 'blah'
+AWS_ROLE_NAME = 'LambdaFunctions'
 AWS_ACCOUNT_NUM = '304434685916'
 AWS_REGION = 'ap-southeast-2'
 AWS_IAM_ROLE = 'arn:aws:iam::' + AWS_ACCOUNT_NUM + ':role/service-role/' + AWS_ROLE_NAME;
